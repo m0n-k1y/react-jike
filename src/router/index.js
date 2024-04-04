632e672f0684c776924305577baa1ea3f41a2928
m0_nk_1y <hcjhcjjjjj@gmail.com>
2024-04-04 22:45:54 +0800

    feat: 路由鉴权实现

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
 //引入路由组件
-import Layout from "@/pages/Layout";
-import Login from "@/pages/Login";
+import Layout from "@/pages/Layout"
+import Login from "@/pages/Login"
+//导入高阶组件
+import AuthRoute from "@/components/AuthRoute"
 
 //配置路由
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />,
+    element: (
+      <AuthRoute>
+        <Layout />
+      </AuthRoute>
+    ),
   },
   {
     path: "/login",
