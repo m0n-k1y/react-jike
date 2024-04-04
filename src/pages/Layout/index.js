215856764db6ab041e3258edd511cf0e8890220e
m0_nk_1y <hcjhcjjjjj@gmail.com>
2024-04-04 22:25:40 +0800

    feat: token注入

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,4 +1,10 @@
+//测试token是否成功注入
+import { request } from "@/utils";
+import { useEffect } from "react";
 const Layout = () => {
+  useEffect(() => { 
+    request.get('/user/profile')
+  },[])
   return <div>Layout</div>;
 };
 
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,24 +1,30 @@
 import axios from "axios"
+import { getToken } from "./token"
 // 创建axios实例
 const request = axios.create({
   baseURL: "http://geek.itheima.net/v1_0",
   timeout: 5000,
 })
 // 添加请求拦截器
+// 添加token校验
 request.interceptors.request.use(
-  config => {
+  (config) => {
+    const token = getToken()
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
     return config
   },
-  error => {
+  (error) => {
     return Promise.reject(error)
   }
 )
 // 添加响应拦截器
 request.interceptors.response.use(
-  response => {
+  (response) => {
     return response.data
   },
-  error => {
+  (error) => {
     return Promise.reject(error)
   }
 )
