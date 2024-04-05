437329d9fab4df59864adcfbc080d7dd2aa2464c
m0_nk_1y <hcjhcjjjjj@gmail.com>
2024-04-05 17:31:21 +0800

    feat: token的失效处理

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ import store from './store';
 import 'normalize.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router}></RouterProvider>
     </Provider>
-  </React.StrictMode>
+  // </React.StrictMode>
 )
 
 
