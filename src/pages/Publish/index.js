be7570fe77f8bf8adf303b7a5c059b2d0e9d8241
m0_nk_1y <hcjhcjjjjj@gmail.com>
2024-04-06 00:21:12 +0800

    feat: 限制图片上传数量

--- src/pages/Publish/index.js
+++ src/pages/Publish/index.js
@@ -128,6 +128,7 @@ const Publish = () => {
                 showUploadList
                 action={"http://geek.itheima.net/v1_0/upload"}
                 onChange={onChange}
+                maxCount={imageType}
               >
                 <div style={{ marginTop: 8 }}>
                   <PlusOutlined />
